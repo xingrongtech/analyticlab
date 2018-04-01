112f941ee63ff947d9bc0c2053989cfa03ab8d90
Wei Xinchong <wzv100@163.com>
2018-04-01 13:25:20 +0800

    source code update of 0.2.3

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 19:15:17 2018
+
+@author: xingrongtech
+"""
+
+from distutils.core import setup
+
+setup(name = 'analyticlab',
+      version = '0.2.3',
+      description='A library for experimental data calculation, treatment and display, which can be used for College Physics Experiment, Analytical Chemistry, etc.', 
+      author = 'xingrongtech',
+      author_email = 'wzv100@163.com',
+      license='MIT License',
+      install_requires = ['numpy', 'scipy', 'sympy'],
+      packages=['analyticlab', 'analyticlab.lookup', 
+                'analyticlab.system', 'analyticlab.uncertainty'],
+      keywords=['calculation', 'analysis', 'display'],
+      url='https://github.com/xingrongtech/analyticlab',
+      classifiers=[
+              'Development Status :: 4 - Beta',
+              'Operating System :: OS Independent',
+              'Intended Audience :: Developers',
+              'License :: OSI Approved :: BSD License',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: Implementation',
+              'Programming Language :: Python :: 3.4',
+              'Programming Language :: Python :: 3.5',
+              'Programming Language :: Python :: 3.6',
+              'Topic :: Software Development :: Libraries'
+      ],
+      )
