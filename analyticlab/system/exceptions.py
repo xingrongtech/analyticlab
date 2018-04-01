bd1f826717dd86adad5ff961c2bc124d807c06c1
Wei Xinchong <wzv100@163.com>
2018-04-01 15:41:57 +0800

    source code update of 0.2.3

---
+++ analyticlab/system/exceptions.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 18:59:21 2018
+
+@author: xingrongtech
+"""
+
+class confLevelUnsupportedException(BaseException):
+    def __init__(self, confLevel, tableType):
+        print('不支持置信度为%s的%s' % (confLevel, tableType))
+        
+class alphaUnsupportedException(BaseException):
+    def __init__(self, alpha, tableType):
+        print('不支持显著性水平为%s的%s' % (alpha, tableType))
+
+class tooMuchDataForTestException(BaseException):
+    def __init__(self, msg):
+        print(msg) 
+
+class tooLessDataForTestException(BaseException):
+    def __init__(self, tableType):
+        print('可供%s的数据个数太少，无法进行%s' % (tableType, tableType))
+        
+class expressionInvalidException(BaseException):
+    def __init__(self, msg):
+        print(msg) 
+        
+class muNotFoundException(BaseException):
+    def __init__(self, msg):
+        print(msg)
+        
+class itemNotSameLengthException(BaseException):
+    def __init__(self, msg):
+        print(msg)
+        
+class itemNotSameTypeException(BaseException):
+    def __init__(self, msg):
+        print(msg)
+        
+class itemNotSameKeysException(BaseException):
+    def __init__(self, msg):
+        print(msg)
+        
+class keyNotInTableException(BaseException):
+    def __init__(self, msg):
+        print(msg)
+        
+class processStateWrongException(BaseException):
+    def __init__(self):
+        print('Uncertainty.process没有初始化为True，不能展示不确定度')
