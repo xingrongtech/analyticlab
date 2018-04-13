f7ba9925d9db72a130d4a0e2245a71527940a567
Wei Xinchong <wzv100@163.com>
2018-04-13 08:31:55 +0800

    source code update of 0.2.5

---
+++ analyticlab/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 18 09:25:00 2018
+
+@author: xingrongtech
+"""
+
+from . import outlier, lookup
+from .num import Num
+from .numitem import NumItem
+from .lsym import LSym
+from .lsymitem import LSymItem
+from .latexoutput import LaTeX, dispTable, dispLSym, dispLSymItem, dispUnc, dispRelErr
+from .const import Const, PI, E, hPercent, t1e, ut1e
+from .amath import sqrt, ln, lg, sin, cos, tan, csc, sec, cot, arcsin, arccos, arctan, arccsc, arcsec, arccot
+from .twoitems import cov, corrCoef, sigDifference
+from .uncertainty import Measure, Uncertainty, ins, Ins, std, ACategory, BCategory
+from .system.numberformat import f, fstr
