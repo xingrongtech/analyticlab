533768d7531c87c7e34d48fb55c34bafe925b51c
Wei Xinchong <wzv100@163.com>
2018-08-13 18:44:30 +0800

    source code update of 0.2.7

--- setup.py
+++ setup.py
@@ -5,20 +5,26 @@ Created on Wed Jan 24 19:15:17 2018
 @author: xingrongtech
 """
 
-from distutils.core import setup
+import setuptools
 
-setup(name = 'analyticlab',
-      version = '0.2.6',
-      description='A library for experimental data calculation, treatment and display, which can be used for College Physics Experiment, Analytical Chemistry, etc.', 
-      author = 'xingrongtech',
-      author_email = 'wzv100@163.com',
-      license='MIT License',
-      install_requires = ['numpy', 'scipy', 'sympy'],
-      packages=['analyticlab', 'analyticlab.lookup', 
-                'analyticlab.system', 'analyticlab.uncertainty'],
-      keywords=['calculation', 'analysis', 'display'],
-      url='https://github.com/xingrongtech/analyticlab',
-      classifiers=[
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="analyticlab",
+    version="0.2.7",
+    author="xingrongtech",
+    author_email="wzv100@163.com",
+    description="A library for experimental data calculation, treatment and display, which can be used for College Physics Experiment, Analytical Chemistry, etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xingrongtech/analyticlab",
+    install_requires = ['numpy', 'scipy', 'sympy'],
+    keywords=['calculation', 'analysis', 'display'],
+    packages=['analyticlab', 'analyticlab.lookup', 
+              'analyticlab.system', 'analyticlab.uncertainty'],
+    license='MIT License',
+    classifiers=(
               'Development Status :: 4 - Beta',
               'Operating System :: OS Independent',
               'Intended Audience :: Developers',
@@ -29,5 +35,5 @@ setup(name = 'analyticlab',
               'Programming Language :: Python :: 3.5',
               'Programming Language :: Python :: 3.6',
               'Topic :: Software Development :: Libraries'
-      ],
-      )
+    ),
+)
