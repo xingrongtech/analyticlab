c020a6a243e73339859933458c5bde89435b1fa5
Wei Xinchong <wzv100@163.com>
2018-08-25 17:49:49 +0800

    source code update of 0.3.1

--- analyticlab/__init__.py
+++ analyticlab/__init__.py
@@ -13,5 +13,5 @@ from .lsymitem import LSymItem
 from .latexoutput import LaTeX, dispTable, dispLSym, dispLSymItem, dispMeasure, dispRelErr
 from .const import Const, PI, E, hPercent, t1e, ut1e
 from .amath import sqrt, ln, lg, sin, cos, tan, csc, sec, cot, arcsin, arccos, arctan, arccsc, arcsec, arccot
-from .twoitems import cov, corrCoef, sigDifference
+from .twoitems import cov, corrCoef, sigDifference, linear_fit
 from .system.numberformat import f, fstr
